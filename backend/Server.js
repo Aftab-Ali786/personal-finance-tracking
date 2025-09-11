eec8d58cb82d6c1b02f528d4acee229740cb9377
Aftab ali <aftabalirizvi123@gmail.com>
2025-09-11 14:03:10 +0530

    backend and database is created

---
+++ backend/Server.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/finance-tracking', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+    // schema
+    const transactionSchema = new mongoose.Schema({
+        title:{ type: String, required: true},
+        amount:{ type: Number, required: true},
+        category:{ type: String, required: true},
+        date:{ type: Date, default: Date.now }
+    });
+
+    const Transaction = mongoose.model('Transaction', transactionSchema);
+
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
